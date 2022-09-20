5bf86fa90e23fd58a1fd357ca27a39c9ed424f7d
Acre9000 <45312776+Acre9000@users.noreply.github.com>
2022-09-20 10:51:44 +0200

    upgrade to urp

---
+++ Assets/Andres/ConfigurableJointManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigurableJointManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public float posSpring;
+    public float posDamper;
+
+    void Start()
+    {
+        //void GetValues();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void GetValues() 
+    {
+        
+    }
+
+}
